3aeaa6a4590ecdbc31017ce0ca5428c4600c4be2
Artutev <artutev@gmail.com>
2023-10-16 19:45:20 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+@app.route('/')
+def home():
+    users = User.query.all()
+    return render_template('home.html', users=users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
